aed2832a58201279235c0d342ae2895d0fff2512
J.C. Beneyto <jnk.beneyto@gmail.com>
2021-02-28 12:58:03 +0100

    commenting code

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 150 });
+
+    // setup and open a browser 
+    const browser = await puppeteer.launch({ headless: false, slowMo: 150 }); //to watch simulation slowly
+    // const browser = await puppeteer.launch({ headless: true }); //to do not watch evolution and faster
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/')
+
+    // fill up the form
     await page.type("#name", "My name")
     await page.type("#email", "email@mail.com")
     await page.select('#selectYears', '2')
     await page.$eval('#exampleCheck1', check => check.checked = true)
-    // await page.click("#windows",{clickCount:1}); //example to check in radio button
     await page.type("#phone", "666555444")
     await page.type("#pwd", "mypassword")
     await page.type("#rpwd", "mypassword")
     await page.click("#enter")
 
+    // evaluate result
     const result = await page.$eval('#result', element => element.textContent);
     if (result === 'SUCCESS') {
         console.log('form working!!')
@@ -21,6 +26,9 @@ const puppeteer = require('puppeteer');
         console.error('There is some error')
     }
 
+    // take a screenshot
     await page.screenshot({ path: 'test.png' });
+
+    //closing browser
     await browser.close();
 })();
